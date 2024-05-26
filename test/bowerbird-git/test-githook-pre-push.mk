6f468b4ba358f64e0833d545c33c855c4c3edeaf
Jeff Fredenburg <jfreden@fnal.gov>
2024-05-26 07:55:05 -0500

    separated the tests into smaller unit tests

--- test/bowerbird-git/test-githook-pre-push.mk
+++ test/bowerbird-git/test-githook-pre-push.mk
@@ -5,23 +5,50 @@ override TEST_FILE.MK := $(lastword $(MAKEFILE_LIST))
 #Targets
 PHONY: test-githook-pre-push
 test-githook-pre-push: \
-		test-githook-pre-push-gitdir \
-		test-githook-pre-push-testdir \
+		test-githook-pre-push-gitdir-file-exists \
+		test-githook-pre-push-gitdir-file-executable \
+		test-githook-pre-push-gitdir-file-contents \
+		test-githook-pre-push-testdir-file-exists \
+		test-githook-pre-push-testdir-file-executable \
+		test-githook-pre-push-testdir-file-contents \
 
-PHONY: test-githook-pre-push-gitdir
-test-githook-pre-push-gitdir: $(WORKDIR_TEST)/test-githook-pre-push-gitdir/hooks/pre-push-expected
+
+PHONY: test-githook-pre-push-gitdir-file-exists
+test-githook-pre-push-gitdir-file-exists:
 	@test -f  $(WORKDIR_GITHOOKS)/pre-push
+	@printf "\e[1;32mPassed: $(TEST_FILE.MK)::$@\e[0m\n"
+
+PHONY: test-githook-pre-push-gitdir-file-executable
+test-githook-pre-push-gitdir-file-executable:
 	@test -x  $(WORKDIR_GITHOOKS)/pre-push
+	@printf "\e[1;32mPassed: $(TEST_FILE.MK)::$@\e[0m\n"
+
+PHONY: test-githook-pre-push-gitdir-file-contents
+test-githook-pre-push-gitdir-file-contents: \
+			$(WORKDIR_TEST)/test-githook-pre-push-gitdir-file-contents/hooks/pre-push-expected
 	@diff $(WORKDIR_GITHOOKS)/pre-push $(WORKDIR_TEST)/$@/hooks/pre-push-expected
 	@printf "\e[1;32mPassed: $(TEST_FILE.MK)::$@\e[0m\n"
 
-PHONY: test-githook-pre-push-testdir
-test-githook-pre-push-testdir: $(WORKDIR_TEST)/test-githook-pre-push-testdir/hooks/pre-push-expected
+
+PHONY: test-githook-pre-push-testdir-file-exists
+test-githook-pre-push-testdir-file-exists:
 	@$(MAKE) $(TARGET_GITHOOKS) WORKDIR_GITHOOKS=$(WORKDIR_TEST)/$@/hooks
 	@test -f $(WORKDIR_TEST)/$@/hooks/pre-push
+	@printf "\e[1;32mPassed: $(TEST_FILE.MK)::$@\e[0m\n"
+
+PHONY: test-githook-pre-push-testdir-file-executable
+test-githook-pre-push-testdir-file-executable:
+	@$(MAKE) $(TARGET_GITHOOKS) WORKDIR_GITHOOKS=$(WORKDIR_TEST)/$@/hooks
 	@test -x $(WORKDIR_TEST)/$@/hooks/pre-push
+	@printf "\e[1;32mPassed: $(TEST_FILE.MK)::$@\e[0m\n"
+
+PHONY: test-githook-pre-push-testdir-file-contents
+test-githook-pre-push-testdir-file-contents: \
+			$(WORKDIR_TEST)/test-githook-pre-push-testdir-file-contents/hooks/pre-push-expected
+	@$(MAKE) $(TARGET_GITHOOKS) WORKDIR_GITHOOKS=$(WORKDIR_TEST)/$@/hooks
 	@diff  $(WORKDIR_TEST)/$@/hooks/pre-push $(WORKDIR_TEST)/$@/hooks/pre-push-expected
 	@printf "\e[1;32mPassed: $(TEST_FILE.MK)::$@\e[0m\n"
+
 
 $(WORKDIR_TEST)/%/pre-push-expected: $(MAKEFILE_LIST)
 	@mkdir -p $(dir $@)
